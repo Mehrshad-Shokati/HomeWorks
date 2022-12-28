4c8a91eb74b866b85a7c1733626028dcce937437
Mehrshad <120194958+Mehrshad-Shokati@users.noreply.github.com>
2022-12-28 21:19:18 +0330

    Delete EX3-1.py

--- Exercises-Session2/EX3-1.py
+++
@@ -1,9 +0,0 @@
-a=int(input("enter the length of List: ")) 
-p=[]
-for i in range(a):
-    s=int(input("Fill the List: "))
-    p.append(s)
-print("The orginal List is: ",p)
-
-p=list(dict.fromkeys(p))
-print(p)
